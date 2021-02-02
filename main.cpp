c4250b2cb42c8e1a29cb2eaaa7e13c656eccefbf
yasmine_khalil <yasminekhalil@aucegypt.edu>
2021-02-02 13:44:46 +0200

    message

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include"max.h"
+#include"knapsack.h"
+int main()
+{
+int val[] = { 60, 100, 120 };
+int wt[] = { 10, 20, 30 };
+int W = 50;
+int n = sizeof(val) / sizeof(val[0]);
+cout << knapSack(W, wt, val, n);
+return 0;
+}
+
+
