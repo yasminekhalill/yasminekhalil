e788451410247fe93e8b4e6f5a07cb68ae76e3fc
MMAAKH2001 <78417402+MMAAKH2001@users.noreply.github.com>
2021-02-02 14:31:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int val[] = { 60, 100, 120 };
 int wt[] = { 10, 20, 30 };
 int W = 50;
 int n = sizeof(val) / sizeof(val[0]);
-cout << knapSack(W, wt, val, n);
+cout << knapsack(W, wt, val, n);
 return 0;
 }
 
