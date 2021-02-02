abff0931b8ecac8c82cf5faf1f222dc8451fe5bb
MMAAKH2001 <78417402+MMAAKH2001@users.noreply.github.com>
2021-02-02 14:29:07 +0200

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -5,5 +5,5 @@ return 0;
 if (wt[n-1]>w)
 return knapsack (w, wt, val, n-1);
 else
-return max(val[n-1]+knapsack(w - wt[n-1], wt, val, n-1), knapsack(w, wt, val, n-1);
+return max(val[n-1]+knapsack(w - wt[n-1], wt, val, n-1), knapsack(w, wt, val, n-1));
 }
