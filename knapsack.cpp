8b267f43816f48082251b702956931328a91aaeb
MMAAKH2001 <17094@stemegypt.edu.eg>
2021-02-02 13:39:31 +0200

    message

---
+++ knapsack.cpp
@@ -0,0 +1,9 @@
+#include "knapsack.h"
+int knapsack(int w, int wt[], int val[], int n){
+if (n == 0 || w == 0)
+return 0;
+if (wt[n-1]>w)
+return knapsack (w, wt, val, n-1);
+else
+return max(val[n-1]+knapsack(w - wt[n-1], wt, val, n-1), knapsack(w, wt, val, n-1);
+}
---
+++ knapsack.h
@@ -0,0 +1,4 @@
+#include <iostream>
+using namespace std;
+int knapSack(int w, int wt[], int val[], int n);
+
